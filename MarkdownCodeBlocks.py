aa1d7ef1363c62904d69d3430576c470dc5de029
Mark Molnar <molnarpw@gmail.com>
2018-09-05 17:58:09 +0200

    Initial commit

---
+++ MarkdownCodeBlocks.py
@@ -0,0 +1,33 @@
+import sublime
+import sublime_plugin
+
+class MarkdownAddCodeBlockCommand(sublime_plugin.TextCommand):
+  _input = None
+  def run(self, edit, key = '', val = '', save = True):
+    if not self._input:
+      self._input = sublime.active_window().show_input_panel('Language Name', '', self.on_done, self.on_change, self.on_close)
+
+    self.on_change(path)
+
+  def on_done(self, language):
+    self.view.run_command('markdown_insert_code_block', {
+      'args': {
+        'language': language
+      }
+    })
+
+  def on_close(self):
+    self._input = None
+
+  def on_change(self):
+    pass
+
+class MarkdownInsertCodeBlockCommand(sublime_plugin.TextCommand):
+  def run(self, edit, args):
+    lang_name = args['language']
+    if not lang_name:
+      lang_name = ''
+
+    self.view.run_command('insert_snippet', {
+      'contents': '```' + lang_name + '\n$0\n```'
+    })
